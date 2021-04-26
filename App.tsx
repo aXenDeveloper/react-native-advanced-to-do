f6ddf31e33e8a1a8645fe7763a5e1e2da263c635
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-04-26 20:15:27 +0200

    Add padding-top for Android and SafeArea for iOS

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Platform } from 'react-native';
 import styled from 'styled-components/native';
 
-const StyledView = styled.View`
+const StyledView = styled.SafeAreaView`
   flex: 1;
   background-color: #fff;
   align-items: center;
   justify-content: center;
+  padding-top: ${Platform.OS === 'android' ? 25 : 0};
 `;
 
 export default function App() {
